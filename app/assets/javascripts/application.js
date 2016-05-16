f27c3202952d52c9b00d2d6b122ee1e854289369
Ayush Jha <ayushjha@apples-MacBook-Pro.local>
2016-05-15 22:41:56 -0700

    changes on index.html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,15 @@ console.log ("JS is loaded & running!!");
 Turbolinks.enableProgressBar();
 
 
+// $("#cardsDiv").sortable({
+//     cancel: ".fixed"
+// });
+// $("#cardsDiv").disableSelection();
 
 // Add methods for drag & drop
 $( ".cardsDiv" ).sortable({
     connectWith: ".cardsDiv",
-    handle: ".panel-title",
+    handle: ".panel-heading",
     cancel: ".panel-toggle",
     start: function (event, ui) {
         ui.item.addClass('tilt');
@@ -52,15 +56,15 @@ function tilt_direction(item) {
     item.data("move_handler", move_handler);
 }
 
-$( ".panel-title" )
+$( ".panel" )
     .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-    .find( ".panel-title" )
+    .find( ".panel-heading" )
     .addClass( "ui-widget-header ui-corner-all" )
     .prepend( "<span class='ui-icon ui-icon-minusthick panel-toggle'></span>");
 
 $( ".panel-toggle" ).click(function() {
     var icon = $( this );
     icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
-    icon.closest( ".panel" ).find( ".panel-content" ).toggle();
+    icon.closest( ".panel" ).find( ".portlet-title" ).toggle();
 });
 // End methods for drag & drop
