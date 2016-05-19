31a73d28385d93fe84470ab8f3f8580c71f0b61f
Nathan L <adamnate@gmail.com>
2016-05-18 17:34:07 -0700

    some more tests passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe User, type: :model do
   describe User do
     it { should have_many(:unit_users) }
   end
-
-
+  describe User do
+    it { should have_many(:user_chores) }
+  end
+  
 
 end
