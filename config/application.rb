ee070866c82673ca654987b866b61b28cf5a178d
altonelli <altonelli14@gmail.com>
2017-01-13 20:11:05 -0800

    more asset stuff

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Chorus
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_files = true
     config.assets.precompile += [
       'application.css',
       'application.js'
